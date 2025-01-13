3fadcff92066586fca7ca05310f2634ce86321c5
dawoodkaleem <dawood9743@gmail.com>
2025-01-13 19:47:41 +0500

    Hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
+// import Herosection from "@/components/Herosection";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     
-         <h1 className="text-2xl text-center">Hello World</h1>
+       <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.02]">
+          
+        <HeroSection/>
+       </main>
   );
 }
