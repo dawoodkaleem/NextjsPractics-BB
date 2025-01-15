d1c1365532e8fbbba98dbcd51cfc7bf648fdfd35
dawoodkaleem <dawood9743@gmail.com>
2025-01-15 10:17:46 +0500

    scroll animation and key frame added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ export default {
     extend: {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -34,6 +35,11 @@ export default {
           "100%": {
             opacity: "1",
             transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
