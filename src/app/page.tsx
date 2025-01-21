7de7be207289c63741a5bbbda40d0a79bcae338f
dawoodkaleem <dawood9743@gmail.com>
2025-01-21 20:18:36 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 // import Herosection from "@/components/Herosection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructurs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -7,6 +13,12 @@ export default function Home() {
        <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.02]">
           
         <HeroSection/>
+        <FeaturedCourses/>
+        <WhyChooseUs/>
+        <MusicSchoolTestimonials/>
+        <UpcomingWebinars/>
+        <Instructors/>
+        <Footer/>
        </main>
   );
 }
